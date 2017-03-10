480ce30b8e5b7691a1e69c5acebed3449b42282c
dbarelop <dbarelop@gmail.com>
2017-03-11 00:28:51 +0100

    Initial commit

---
+++ src/main/java/es/ucode/oesia/random/SocialmediatorApplication.java
@@ -0,0 +1,12 @@
+package es.ucode.oesia.random;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialmediatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialmediatorApplication.class, args);
+	}
+}
