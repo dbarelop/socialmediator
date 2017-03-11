800e7c5f058732cc8dc87ef77c0c7771ee1d229c
dbarelop <dbarelop@gmail.com>
2017-03-11 09:46:56 +0100

    Moved AngularJS app to separate file

---
+++ src/main/resources/static/js/app/app.js
@@ -0,0 +1,32 @@
+angular.module("app", []).controller("home", function($http, $location, $window) {
+    var self = this;
+    $http.get("/user").success(function(data) {
+        self.user = data.userAuthentication.details.name;
+        self.authenticated = true;
+        $http.get("/user/socialnetworks").success(function(data) {
+            self.socialNetworks = data;
+        }).error(function() {
+            console.log("Error getting user's social networks");
+        });
+    }).error(function() {
+        self.user = "N/A";
+        self.authenticated = false;
+    });
+    self.logout = function() {
+        $http.post("/logout", {}).success(function() {
+            self.authenticated = false;
+            $location.path("/");
+        }).error(function(data) {
+            console.log("Logout failed");
+            self.authenticated = false;
+        });
+    };
+    self.authorizeTwitter = function() {
+        $http.get("/auth/twitter").success(function(data) {
+            $window.location.href = data.url;
+        }).error(function(data) {
+            console.log(data);
+            console.log("Twitter auth failed");
+        });
+    };
+});
