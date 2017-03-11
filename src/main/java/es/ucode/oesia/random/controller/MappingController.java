60bf0cf82070252e3f2f4c380574e61fc78418a9
dbarelop <dbarelop@gmail.com>
2017-03-11 12:47:29 +0100

    Added sample visualization for posts

--- src/main/java/es/ucode/oesia/random/controller/MappingController.java
+++ src/main/java/es/ucode/oesia/random/controller/MappingController.java
@@ -4,10 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class MappingController {
 
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/feed")
+    public String feed() {
+        return "feed";
     }
 }
--- src/main/resources/static/js/app/app.js
+++ src/main/resources/static/js/app/app.js
@@ -1,32 +1,42 @@
-angular.module("app", []).controller("home", function($http, $location, $window) {
-    var self = this;
-    $http.get("/user").success(function(data) {
-        self.user = data.userAuthentication.details.name;
-        self.authenticated = true;
-        $http.get("/user/socialnetworks").success(function(data) {
-            self.socialNetworks = data;
+angular.module('app', [])
+    .controller('home', ['$http', '$location', '$window', function($http, $location, $window) {
+        var self = this;
+        $http.get('/user').success(function(data) {
+            self.user = data.userAuthentication.details.name;
+            self.authenticated = true;
+            $http.get('/user/socialnetworks').success(function(data) {
+                self.socialNetworks = data;
+            }).error(function() {
+                console.log('Error getting user\'s social networks');
+            });
         }).error(function() {
-            console.log("Error getting user's social networks");
-        });
-    }).error(function() {
-        self.user = "N/A";
-        self.authenticated = false;
-    });
-    self.logout = function() {
-        $http.post("/logout", {}).success(function() {
-            self.authenticated = false;
-            $location.path("/");
-        }).error(function(data) {
-            console.log("Logout failed");
+            self.user = 'N/A';
             self.authenticated = false;
         });
-    };
-    self.authorizeTwitter = function() {
-        $http.get("/auth/twitter").success(function(data) {
-            $window.location.href = data.url;
-        }).error(function(data) {
-            console.log(data);
-            console.log("Twitter auth failed");
+        self.logout = function() {
+            $http.post('/logout', {}).success(function() {
+                self.authenticated = false;
+                $location.path('/');
+            }).error(function(data) {
+                console.log('Logout failed');
+                self.authenticated = false;
+            });
+        };
+        self.authorizeTwitter = function() {
+            $http.get('/auth/twitter').success(function(data) {
+                $window.location.href = data.url;
+            }).error(function(data) {
+                console.log(data);
+                console.log('Twitter auth failed');
+            });
+        };
+    }])
+    .controller('feed', ['$http', '$location', function($http, $location) {
+        var self = this;
+        var social_network = $location.search().social_network;
+        $http.get(social_network ? '/posts/' + social_network + '/latest' : '/posts/latest').success(function(data) {
+            self.posts = data;
+        }).error(function() {
+            console.log('Error getting posts');
         });
-    };
-});
+    }]);
